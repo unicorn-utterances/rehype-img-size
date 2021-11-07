c5cbef6c5396b8ff04d99b723f079423722ff9e8
Corbin Crutchley <crutchcorn@gmail.com>
2021-11-06 21:49:53 -0700

    Add TS types

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+import type {Root} from 'hast'
+interface Options {
+  dir: string;
+}
+import type {Plugin} from 'unified'
+
+// Note: defining all nodes here, such as with `Root | Element | ...` seems
+// to trip TS up.
+declare const rehypeImgSize: Plugin<[Options] | [], Root, string>
+export default rehypeImgSize
+export type {Options}
